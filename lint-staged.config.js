b0c2f128306e60cf3cba50ed161f49649306aaef
Marijn <m.wijbenga@outlook.com>
2023-07-13 10:06:31 +0200

    prettier fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '{apps,libs}/**/*.{ts,tsx, html}': files => {
     return `nx affected --target=typecheck --files=${files.join(',')}`;
   },
-  '{apps,libs}/**/*.{js,ts,jsx,tsx,json}': [
+  '{apps,libs}/**/*.{js,ts,jsx,tsx,json, html}': [
     files => `nx affected:lint --files=${files.join(',')} --fix`,
     files => `nx format:write --files=${files.join(',')}`,
   ],
